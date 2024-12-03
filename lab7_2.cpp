737ecf472c311383b6571525d0b98845f88a4276
sriv95 <srivichai95@gmail.com>
2024-12-03 15:57:50 +0700

    lab7 complete

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -16,3 +16,34 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars V
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    string name;
+    int id;
+    string movie;
+    string day;
+    string text;
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
+    cout << "?????: ";
+    getline(cin,name);
+    cout << "Fahsai: Wow!!! "<<name<<" is a really cool name.\nFahsai: I think you are an Engineering student. What is your student ID?\n";
+    cout << name << ": ";
+    cin >> id;
+    cin.ignore();
+    cout << "Fahsai: I think you may be GEAR "<<floor(id/pow(10,7))-12<<". I have a free movie ticket for you.\n";
+    cout << "Fahsai: Let's go to the cinema together!!!\n";
+    cout << "Fahsai: What movie do you want to watch?\n";
+    cout << name << ": ";
+    getline(cin,movie);
+    cout << "Fahsai: So....which day are you free to go with me?\n";
+    cout << name << ": ";
+    getline(cin,day);
+    cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching "<<movie<<" with you.\n";
+    cout << name<<": ";
+    getline(cin,text);
+    cout << "Fahsai: 555+ see you "<<day<<". Bye Bye \\(^ ^)/\n";
+    return 0;
+}
--- lab7_3.cpp
+++ lab7_3.cpp
@@ -3,6 +3,13 @@ using namespace std;
 
 char before(char x){
 	//Write your function definition here
+	if (x=='A'){
+		return 'Z';
+	} else if (x>'A'&&x<='Z'){
+		return x-1;
+	} else{
+		return '0';
+	}
 }
 
 int main(){
